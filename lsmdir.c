ec1de4cd64ad8ee4d74db33b94a10a115974dbc3
DiogoCampos <d.diogocampos@gmail.com>
2016-04-12 15:40:31 +0100

    Listing files recursively and ordering them by name

--- lsmdir.c
+++ lsmdir.c
@@ -5,76 +5,70 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
     DIR * dirp;
-    
+
     struct dirent *d;
     struct stat s;
-    
+    pid_t pid;
+
     if (argc != 2)
     {
-        printf("Argumentos nao validos\n");
-        return 1;
+        printf("Usage: %s <directory>\n", argv[0]);
+        exit(1);
     }
-    
+
     dirp = opendir(argv[1]);
-    
+
     if (dirp == NULL)
     {
-        printf("Diretorio nao existente\n");
-        return 2;
+        perror(argv[1]);
+        exit(2);
     }
-    
-    while((d = readdir( dirp)) != NULL)
-    {
 
+    while((d = readdir(dirp)) != NULL)
+    {
         char n[200];
-        sprintf(n,"%s/%s",argv[1],d->d_name);
-        
+        sprintf(n, "%s/%s", argv[1], d->d_name);
+
         if (lstat(n, &s) == -1)
         {
-            printf("Erro ao definir a stat\n");
-            printf("%s\n", argv[1]);
-            return 3;
+            perror(n);
+            exit(3);
         }
-        
-        if (S_ISREG(s.st_mode)) 
+
+        if (!(d->d_name[0] == '.' || strcmp(d->d_name, "files.txt") == 0 || strcmp(d->d_name, "filestemp.txt") == 0))
         {
-            int f = open("files.txt",  O_WRONLY | O_CREAT | O_EXCL , S_IRUSR | S_IWUSR | S_IXUSR);
-            sprintf(n, "%-25s %-20s %-8d %-10d %-10d\n", d->d_name, argv[1], (int)s.st_size, s.st_mode, (int)s.st_mtime);
-            dup2(f,STDOUT_FILENO);
-            printf("%s", n);
-            close(f);
-        }
-        else if (S_ISDIR(s.st_mode))
-        {
-            if (!((strcmp(d->d_name, ".") == 0) || (strcmp(d->d_name, "..")  == 0) || (strcmp(d->d_name, ".git")  == 0)))
-            {
-                if (fork() == 0)
-                {
-                    
-                    execl("./lsmdir","./lsmdir", n , NULL);
-                }
-            }
-        }
-        else
-        {
-            printf(" %-20s - %s\n", d->d_name, "outro");
+          if (S_ISREG(s.st_mode))
+          {
+              int f = open("filestemp.txt", O_WRONLY | O_APPEND);
+              sprintf(n, "%-25s %-20s %-8d %-10d %-10d\n", d->d_name, argv[1], (int)s.st_size, s.st_mode, (int)s.st_mtime);
+              dup2(f, STDOUT_FILENO);
+              printf("%s", n);
+              close(f);
+          }
+          else if (S_ISDIR(s.st_mode))
+          {
+              if ((pid = fork()) < 0) {
+                perror("fork");
+                exit(4);
+              }
+
+              else if (pid == 0) {
+                  execl("./lsmdir","./lsmdir", n , NULL);
+              }
+
+              else {
+                waitpid(pid, NULL, 0);
+              }
+          }
         }
     }
-    
-    int f = open("files.txt", O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR);
-    dup2(f,STDIN_FILENO);
-    close(f);
-    f = open("files.txt", O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR);
-    dup2(f,STDOUT_FILENO);
-    execlp("sort", "sort", "files.txt", NULL);
-    close(f);
-    
+
     closedir(dirp);
     return 0;
 }
--- rmdup.c
+++ rmdup.c
@@ -1,3 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define READ 0
+#define WRITE 1
+
+int sortFile(char filename[])
+{
+  int fd[2];
+  char buf[200];
+  int nr, nw;
+  pid_t pid;
+
+  if (pipe(fd) != 0) {
+    exit(1);
+  }
+
+  if ((pid = fork()) < 0) {
+    perror("fork");
+    exit(2);
+  }
+
+  if (pid > 0) {
+    close(fd[READ]);
+    int f = open(filename, O_RDONLY, S_IRUSR | S_IWUSR | S_IXUSR);
+    while ((nr = read(f, buf, 200)) > 0) {
+      if ((nw = write(fd[WRITE], buf, nr)) <= 0 || nw != nr) {
+        exit(3);
+      }
+    }
+    close(f);
+    close(fd[WRITE]);
+  }
+  else {
+    close(fd[WRITE]);
+    dup2(fd[READ], STDIN_FILENO);
+    close(fd[READ]);
+    int f = open("files.txt", O_RDWR | O_CREAT , S_IRUSR | S_IWUSR | S_IXUSR);
+    dup2(f, STDOUT_FILENO);
+    execlp("sort", "sort", (char*) NULL);
+    close(f);
+  }
+
+  return 0;
+}
+
 int main(int argc, char* argv[]) {
-  
+  int f;
+  pid_t pid;
+
+  if (argc != 2)
+  {
+      printf("Usage: %s <directory>\n", argv[0]);
+      exit(1);
+  }
+
+  if ((f = open("files.txt", O_CREAT | O_TRUNC, S_IRWXU)) == -1)
+  {
+      perror("files.txt");
+      exit(2);
+  }
+
+  if ((f = open("filestemp.txt", O_CREAT | O_TRUNC, S_IRWXU)) == -1)
+  {
+      perror("filestemp.txt");
+      exit(3);
+  }
+
+  if ((pid = fork()) < 0) {
+      perror("fork");
+      exit(4);
+  }
+
+  if (pid == 0) {
+      execl("./lsmdir","./lsmdir", argv[1], NULL);
+  }
+
+  else {
+      waitpid(pid, NULL, 0);
+      sortFile("filestemp.txt");
+      remove("filestemp.txt");
+  }
+
+  return 0;
 }
